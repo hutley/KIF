22f01b195647a2b176913c2cac814cc60392c1b8
Hiedi Utley <hutley@zappos.com>
2013-12-04 15:10:50 -0800

    fix

--- Classes/KIFUITestActor.m
+++ Classes/KIFUITestActor.m
@@ -394,7 +394,7 @@
          KIFTestCondition(pickerView, error, @"No picker view is present");
 
          NSInteger componentCount = [pickerView.dataSource numberOfComponentsInPickerView:pickerView];
-         KIFTestCondition(componentCount == 1, error, @"The picker view has multiple columns, which is not supported in testing.");
+         //KIFTestCondition(componentCount == 1, error, @"The picker view has multiple columns, which is not supported in testing.");
 
          for (NSInteger componentIndex = 0; componentIndex < componentCount; componentIndex++)
          {
